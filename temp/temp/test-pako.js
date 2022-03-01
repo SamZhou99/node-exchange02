731d3f03871463058b073553c6ceebcdafeb02c0
sam.z <45636987@qq.com>
2022-03-01 20:53:20 +0800

    update

--- temp/temp/test-pako.js
+++
@@ -1,5 +0,0 @@
-const pako = require('pako');
-const test = { my: 'super', puper: [456, 567], awesome: 'pako' };
-const compressed = pako.deflate(JSON.stringify(test));
-const restored = JSON.parse(pako.inflate(compressed, { to: 'string' }));
-console.log(restored)
--- temp/temp/test-time.js
+++
@@ -1,3 +0,0 @@
-let utils99 = require('node-utils99')
-
-console.log(utils99.Timestamp(1636715521100))
